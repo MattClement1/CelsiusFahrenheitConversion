30b342976c3a43c01d4afa4bba070cda8f9adc8b
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-10-26 14:33:02 -0400

    Celsius to Fahrenheit

---
+++ CelsiusFahrenheitConversion/Conversion.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace CelsiusFahrenheitConversion
+{
+    partial class frmConversion
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.txtCelsius = new System.Windows.Forms.TextBox();
+            this.lblAnswerText = new System.Windows.Forms.Label();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(21, 13);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(198, 16);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Enter the temperature in Celsius:";
+            // 
+            // txtCelsius
+            // 
+            this.txtCelsius.Location = new System.Drawing.Point(70, 48);
+            this.txtCelsius.Name = "txtCelsius";
+            this.txtCelsius.Size = new System.Drawing.Size(100, 20);
+            this.txtCelsius.TabIndex = 1;
+            // 
+            // lblAnswerText
+            // 
+            this.lblAnswerText.AutoSize = true;
+            this.lblAnswerText.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswerText.Location = new System.Drawing.Point(19, 144);
+            this.lblAnswerText.Name = "lblAnswerText";
+            this.lblAnswerText.Size = new System.Drawing.Size(202, 16);
+            this.lblAnswerText.TabIndex = 2;
+            this.lblAnswerText.Text = "The temperature in Fahrenheit is:";
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.Location = new System.Drawing.Point(106, 169);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(0, 16);
+            this.lblAnswer.TabIndex = 3;
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(81, 74);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // frmConversion
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(240, 209);
+            this.Controls.Add(this.btnCalculate);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.lblAnswerText);
+            this.Controls.Add(this.txtCelsius);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "frmConversion";
+            this.Text = "Celsius Fahrenheit Conversion";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.TextBox txtCelsius;
+        private System.Windows.Forms.Label lblAnswerText;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.Button btnCalculate;
+    }
+}
+
---
+++ CelsiusFahrenheitConversion/Conversion.cs
@@ -0,0 +1,51 @@
+﻿/*
+*Created by: Matthew Clement
+*Created on: 26-Oct-2015
+*Created for: Unit #4-01
+*This program converts Celsius to Fahrenheit.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CelsiusFahrenheitConversion
+{
+    public partial class frmConversion : Form
+    {
+        public void TempFahrenheit(int tempCelsius)
+        {
+            //This procedure converts Celsius to Fahrenheit
+
+            //Variable
+            double tempFahrenheit;
+
+            tempFahrenheit = (9.0 / 5.0) * tempCelsius + 32;
+            this.lblAnswer.Text = Convert.ToString(tempFahrenheit) + "°";
+        }
+
+        public frmConversion()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            //Get the temperature in Celsius and pass it on to the TempFahrenheit procedure.
+
+            //Variable
+            int celsius;
+
+            celsius = Convert.ToInt32(this.txtCelsius.Text);
+
+            TempFahrenheit(celsius);
+
+        }
+    }
+}
